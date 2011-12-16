7114f88ce7215799d4495a98b032781fd6d52bab
bessog <gbesso@vmware.com>
2011-12-15 22:26:19 -0500

    day closing

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Overview
 ```
-This is the repository for rewserv.cloudfoundry.com.
-It is a node.js/express/mongodb application.
+This is the repository for rewserv.cloudfoundry.com and the data collection cron job.
+
+rewsers is a node.js/express/mongodb application.
 This application will provide activity stats per community user.
 
 Planned activity stats:
@@ -9,7 +10,7 @@ Planned activity stats:
   * commits on springsource projects on github
 
 There's a javascript that will collect stats in real time from form submissions
-on the client browsers (see public/javascripts)
+on the client browsers (see rewserv/public/javascripts)
 
 A separate cron job will collect stats.
 ```
--- rewserv-cron/github2rewserv.rb
+++ rewserv-cron/github2rewserv.rb
@@ -8,40 +8,65 @@ require "net/http"
 
 
 
-url = 'https://api.github.com/orgs/SpringSource/repos'
-result = JSON.parse(open(url).read)
+orgs = ['SpringSource','cloudfoundry']
 
-result.each do|repo|
-#    puts repo['name']
-#    puts "\n"
-#    url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/collaborators'
-#    collres = JSON.parse(open(url).read)
-#    url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/contributors'
-#    contres = JSON.parse(open(url).read)
+orgs.each do|org|
 
-    url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/commits'
+
+    result = JSON.parse(open('https://api.github.com/orgs/' + org + '/repos?page=0per_page=200').read)
+
+    result.each do|repo|
+#        puts repo['name']
+#        puts "\n"
+#        url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/collaborators'
+#        collres = JSON.parse(open(url).read)
+#        url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/contributors'
+#        contres = JSON.parse(open(url).read)
+
+        page = 0
+        while done do
+            grabCommit(page)
+
+            url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/commits?page=' + page + 'per_page=200'
+            commres = JSON.parse(open(url).read)
+
+            commres.each do|comm|
+                if comm['commit']['committer'].has_key?('email') or !comm['commit']['committer']['email'].empty? then person = comm['commit']['committer']['email']
+                else person = comm['committer']['login']
+                end
+                puts org + "\t" + repo['name'] + "\t" + person 
+            end
+            test = page + 1
+            url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/commits?page=' + test + 'per_page=200'
+            commres = JSON.parse(open(url).read)
+        end
+#        joindate=Time.at(row['joindate'].to_i).strftime("%Y-%m-%d %H:%M:%S")
+        #comUser = {"email" => row['email'], "joindate" => joindate, "posts" => row['posts']}
+        #puts "echo \"email="+row['email']+"&joindate="+joindate+"&posts="+row['posts']+"\"\r"
+        #command = "/usr/bin/curl --data-ascii \"email="+row['email']+"&joindate="+joindate+"&posts="+row['posts']+"\" http://rew_serv.cloudfoundry.com/\r"
+       ## command = "/usr/bin/curl -d \"email="+row['email']+"&joindate="+joindate+"&posts="+row['posts']+"\" http://rew_serv.cloudfoundry.com/\r"
+       ## puts command+"\r"
+       ## puts `#{command}`
+        #puts "echo \"#############################################################################\"\r"
+        #coll.insert(comUser)
+#        params = {'email'=>row['email'],'joindate'=>joindate,'posts'=>row['posts']}
+#        x = Net::HTTP.post_form(URI.parse('http://rewserv.cloudfoundry.com/activity'), params)
+#        puts x.body
+    end
+    #=end
+
+end
+
+def grabCommit(page)
+    url = 'https://api.github.com/repos/SpringSource/' + repo['name'] + '/commits?page=' + page + 'per_page=200'
     commres = JSON.parse(open(url).read)
 
-    commres.each do|comm|
-        puts repo['name'] + ' ' + comm['commit']['committer']['email']
-        puts "\n"
-
+    commres.each do|comm| 
+        if comm['commit']['committer'].has_key?('email') or !comm['commit']['committer']['email'].empty? then person = comm['commit']['committer']['email']
+        else person = comm['committer']['login']
+        end
+        puts org + "\t" + repo['name'] + "\t" + person
     end
-
-#    joindate=Time.at(row['joindate'].to_i).strftime("%Y-%m-%d %H:%M:%S")
-    #comUser = {"email" => row['email'], "joindate" => joindate, "posts" => row['posts']}
-    #puts "echo \"email="+row['email']+"&joindate="+joindate+"&posts="+row['posts']+"\"\r"
-    #command = "/usr/bin/curl --data-ascii \"email="+row['email']+"&joindate="+joindate+"&posts="+row['posts']+"\" http://rew_serv.cloudfoundry.com/\r"
-   ## command = "/usr/bin/curl -d \"email="+row['email']+"&joindate="+joindate+"&posts="+row['posts']+"\" http://rew_serv.cloudfoundry.com/\r"
-   ## puts command+"\r"
-   ## puts `#{command}`
-    #puts "echo \"#############################################################################\"\r"
-    #coll.insert(comUser)
-#    params = {'email'=>row['email'],'joindate'=>joindate,'posts'=>row['posts']}
-#    x = Net::HTTP.post_form(URI.parse('http://rewserv.cloudfoundry.com/activity'), params)
-#    puts x.body
 end
-#=end
-
 
 
